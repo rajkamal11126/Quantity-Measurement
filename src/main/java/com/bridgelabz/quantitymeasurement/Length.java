cd3ce7dadaf9d31850cb2383555c51111a2efcb8
Raj <rajkamal11126@gmail.com>
2020-12-29 19:23:36 +0530

    Refactor: Functionality to test Dry was violated with new length Entity

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/Length.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.quantitymeasurement;
+
+public class Length {
+	public enum Unit {
+		FEET, INCH
+	}
+
+	private final double value;
+	private final Unit unit;
+
+	public Length(Unit unit, double value) {
+		this.unit = unit;
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Length length = (Length) o;
+		return Double.compare(length.value, value) == 0 && unit == length.unit;
+	}
+}
---
+++ src/test/java/com/bridgelabz/quantitymeasurementtest/LengthTest.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.quantitymeasurementtest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.quantitymeasurement.Length;
+
+public class LengthTest {
+	@Test
+    public void given0FeetAnd0Feet_ShouldReturnTrue() {
+        Length feet1 = new Length(Length.Unit.FEET, 0.0);
+        Length feet2 = new Length(Length.Unit.FEET,0.0);
+        Assert.assertEquals(feet1,feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,1.0);
+        boolean result=feet1.equals(feet2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void given0InchAnd0Inch_ShouldReturnTrue() {
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,0.0);
+        Assert.assertEquals(inch1,inch2);
+    }
+
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEqual() {
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,1.0);
+        boolean result=inch1.equals(inch2);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void given1FeetAnd1Inch_ShouldReturnNotEqual() {
+        Length feet = new Length(Length.Unit.FEET,0.0);
+        Length inch = new Length(Length.Unit.INCH,0.0);
+        Assert.assertNotEquals(feet,inch);
+    }
+}
