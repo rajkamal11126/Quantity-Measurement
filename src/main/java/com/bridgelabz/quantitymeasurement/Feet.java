991eb95a3e7de87263c355498d4df78080f17de8
Raj <rajkamal11126@gmail.com>
2020-12-29 16:53:37 +0530

    Added: Functionality to check equality of inch

--- src/main/java/com/bridgelabz/quantitymeasurement/Feet.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/Feet.java
@@ -3,20 +3,22 @@ package com.bridgelabz.quantitymeasurement;
 public class Feet {
 	private final double value;
 
-    public Feet(double value) {
-       this.value=value; 
-    }
+	public Feet(double value) {
+		this.value = value;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Feet feet = (Feet) o;
-        return Double.compare(feet.value, value) == 0;
-    }
+	@Override
+	public boolean equals(Object object) {
+		if (this == object)
+			return true;
+		if (object == null || getClass() != object.getClass())
+			return false;
+		Feet feet = (Feet) object;
+		return Double.compare(feet.value, value) == 0;
+	}
 
-    @Override
-    public int hashCode() {
-        return 0;
-    }
+	@Override
+	public int hashCode() {
+		return 0;
+	}
 }
--- src/test/java/com/bridgelabz/quantitymeasurementtest/FeetTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurementtest/FeetTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.bridgelabz.quantitymeasurement.Feet;
+import com.bridgelabz.quantitymeasurement.Inch;
 
 public class FeetTest {
 	@Test
@@ -11,6 +12,19 @@ public class FeetTest {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
-
     }
+	 @Test
+	    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+	        Feet feet1 = new Feet(0.0);
+	        Feet feet2 = new Feet(1.0);
+	        boolean result=feet1.equals(feet2);
+	        Assert.assertFalse(result);
+	    }
+
+	    @Test
+	    public void given0InchAnd0Inch_ShouldReturnTrue() {
+	        Inch inch1 = new Inch(0.0);
+	        Inch inch2 = new Inch(0.0);
+	        Assert.assertEquals(inch1,inch2);
+	    }
 }
