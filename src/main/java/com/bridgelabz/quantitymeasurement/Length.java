0a05647052b7e3767a2374a1bd613fa3f9c1a4bd
Raj <rajkamal11126@gmail.com>
2020-12-29 19:45:20 +0530

    Added: Functionality to test and code comparing inches and feet

--- src/main/java/com/bridgelabz/quantitymeasurement/Length.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/Length.java
@@ -1,6 +1,17 @@
 package com.bridgelabz.quantitymeasurement;
 
 public class Length {
+	
+	private static final double FEET_TO_INCH = 12.0;
+
+    public boolean compare(Length that) {
+
+        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.FEET))
+            return Double.compare(this.value, that.value) == 0;
+        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
+            return Double.compare(this.value * FEET_TO_INCH, that.value) == 0;
+        return false;
+    }
 	public enum Unit {
 		FEET, INCH
 	}
